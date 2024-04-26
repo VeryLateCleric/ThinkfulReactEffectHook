08ef9e3eaf3b4b52049ed95af007e03f48867187
Kenneth <kennethyoung.2960@gmail.com>
2024-04-26 04:34:13 -0500

    Needs cancel pending api call and display album list when selecting user. Good night

--- 03-React-API-w-Hooks/React_and_APIs_with_hooks_Assessment_React_18_1/src/App.js
+++ 03-React-API-w-Hooks/React_and_APIs_with_hooks_Assessment_React_18_1/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 import AlbumList from "./AlbumList";
 import UserList from "./UserList";
@@ -7,16 +8,72 @@ import UserList from "./UserList";
 function App() {
   const [users, setUsers] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+  const [albums, setAlbums] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    document.title = "Awesome Album App";
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const usersResponse = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(usersResponse.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      document.title = "React Album App";
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchAlbums = async () => {
+      setLoading(true);
+      if (currentUser.id) {
+        try {
+          const albumsResponse = await axios.get(
+            `https://jsonplaceholder.typicode.com/albums?userId=${currentUser.id}`
+          );
+          setAlbums(albumsResponse.data);
+        } catch (error) {
+          console.error("Error fetching albums:", error);
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        setAlbums([]);
+      }
+    };
+
+    fetchAlbums();
+
+    return () => {
+      setAlbums([]);
+    };
+  }, [currentUser]);
 
   // Load data from https://jsonplaceholder.typicode.com/albums?userId=${user.id}
 
   return (
     <div className="App">
       <div className="left column">
-        <UserList users={users} setCurrentUser={setCurrentUser} />
+        <UserList users={users} setCurrentUser={setCurrentUser} loading={loading} />
       </div>
       <div className="right column">
-        <AlbumList user={currentUser} />
+      {currentUser.id ? (
+        <AlbumList albums={albums} loading={loading} />
+        ) : (
+          loading ? <p>Loading...</p> : <p>Please click on a user name to the left</p>
+        )}
       </div>
     </div>
   );
